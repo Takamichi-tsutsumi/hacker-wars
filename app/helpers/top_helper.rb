e6725fbc2173c32f44e0cc151fe61f2c2507ee22
Hiro Nonoyama <H@Hiro-no-MacBook-Pro.local>
2015-11-29 05:16:54 +0900

    create top.helper.rb

--- app/helpers/top_helper.rb
+++ app/helpers/top_helper.rb
@@ -1,2 +1,16 @@
 module TopHelper
+
+	def user_favorite_check(user, photo_id)
+		if user
+			if Favorite.where(user_id:user.id).where(photo_id:photo_id).exist?
+				return true
+			else
+				return false
+			end
+		else
+			return false
+		end
+	end
+
+	
 end
