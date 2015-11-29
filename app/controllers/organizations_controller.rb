c51eaf245c0e54a5f46490d3a3a8fbc622dcb468
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2015-11-29 15:30:01 +0900

    image fix

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -4,7 +4,8 @@ class OrganizationsController < ApplicationController
     photos = @organization.photos
     @images = {}
     Category.all.each do |category|
-    	@images[category.name.to_s] = photos.where(category_id: category.id).first
+    	@images[category.name.to_s] = photos.where(category_id: category.id).first unless category.name.to_s == 'site'
+    	@images[category.name.to_s] = photos.where(category_id: category.id).second if category.name.to_s == 'site'
     end
   end
 end
