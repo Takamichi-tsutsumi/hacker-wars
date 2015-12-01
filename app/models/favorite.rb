94efe8eb2748a0c54e3b7f4b56f7b002c1867373
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2015-12-01 19:05:58 +0900

    validation update

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -10,7 +10,7 @@
 #
 # お気に入りモデル
 class Favorite < ActiveRecord::Base
-  validates :user_id, presence: true
+  validates :user_id, presence: true, uniqueness: { scope: :photo_id }
   validates :photo_id, presence: true
 
 	belongs_to :user
